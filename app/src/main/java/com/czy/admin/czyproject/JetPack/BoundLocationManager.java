88beed8097ea8fed0656ba55abb16450374ba2e7
cpsir <504500534@qq.com>
2019-03-03 01:13:37 +0800

    JetPack

---
+++ app/src/main/java/com/czy/admin/czyproject/JetPack/BoundLocationManager.java
@@ -0,0 +1,72 @@
+package com.czy.admin.czyproject.JetPack;
+
+import android.Manifest;
+import android.arch.lifecycle.Lifecycle;
+import android.arch.lifecycle.LifecycleObserver;
+import android.arch.lifecycle.LifecycleOwner;
+import android.arch.lifecycle.OnLifecycleEvent;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+
+/**
+ * Created by cpsir on 2019/3/2
+ */
+public class BoundLocationManager implements LifecycleObserver {
+    public static void bindLocationListenerIn(LifecycleOwner lifecycleOwner,
+                                              LocationListener listener, Context context) {
+        new BoundLocationListener(lifecycleOwner, listener, context);
+    }
+
+    @SuppressWarnings("MissingPermission")
+    static class BoundLocationListener implements LifecycleObserver {
+        private final Context mContext;
+        private LocationManager mLocationManager;
+        private final LocationListener mListener;
+
+        public BoundLocationListener(LifecycleOwner lifecycleOwner,
+                                     LocationListener listener, Context context) {
+            mContext = context;
+            mListener = listener;
+            lifecycleOwner.getLifecycle().addObserver(this);
+        }
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+        void onCreate(){
+            Log.i("CZYAPP","i am onCreate");
+        }
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+        void addLocationListener() {
+            // Note: Use the Fused Location Provider from Google Play Services instead.
+            // https://developers.google.com/android/reference/com/google/android/gms/location/FusedLocationProviderApi
+Log.i("CZYAPP","i am resume");
+            mLocationManager =
+                    (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mListener);
+            Log.d("BoundLocationMgr", "Listener added");
+
+            // Force an update with the last location, if available.
+            Location lastLocation = mLocationManager.getLastKnownLocation(
+                    LocationManager.GPS_PROVIDER);
+            if (lastLocation != null) {
+                mListener.onLocationChanged(lastLocation);
+            }
+        }
+
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+        void removeLocationListener() {
+            if (mLocationManager == null) {
+                return;
+            }
+            mLocationManager.removeUpdates(mListener);
+            mLocationManager = null;
+            Log.i("CZYAPP","i am pause");
+        }
+    }
+}

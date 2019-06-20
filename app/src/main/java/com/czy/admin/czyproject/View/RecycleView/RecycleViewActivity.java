db61cdf86f25b04fe2f1bc03ad530136801ec468
cpsir <504500534@qq.com>
2019-06-20 16:48:24 +0800

    RecyclerView

--- app/src/main/java/com/czy/admin/czyproject/View/RecycleView/RecycleViewActivity.java
+++ app/src/main/java/com/czy/admin/czyproject/View/RecycleView/RecycleViewActivity.java
@@ -19,15 +19,14 @@ import java.util.List;
  * Created by czy on 2017/4/26.
  */
 
-public class RecycleViewActivity extends Activity{
+public class RecycleViewActivity extends Activity {
 
     private RecyclerView mRecyclerView;
     private List<String> mDatas;
     private HomeAdapter mAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recyclerview);
         initData();
@@ -38,49 +37,57 @@ public class RecycleViewActivity extends Activity{
     }
 
 
-
-    protected void initData()
-    {
+    protected void initData() {
         mDatas = new ArrayList<String>();
-        for (int i = 'A'; i < 'z'; i++)
-        {
+        for (int i = 'A'; i < 'z'; i++) {
             mDatas.add("" + (char) i);
         }
     }
 
 
-    class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder>
-    {
+    class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder> {
 
 
+        /**
+         * 这个方法主要生成为每个Item inflater出一个View，但是该方法返回的是一个ViewHolder。
+         * 该方法把View直接封装在ViewHolder中，然后我们面向的是ViewHolder这个实例，当然这个ViewHolder需要我们自己去编写。
+         *
+         * @param parent
+         * @param viewType
+         * @return
+         */
         @Override
-        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
-        {
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             MyViewHolder holder = new MyViewHolder(LayoutInflater.from(
                     RecycleViewActivity.this).inflate(R.layout.item_home, parent,
                     false));
             return holder;
         }
 
+        /**
+         * 这个方法主要用于适配渲染数据到View中。方法提供给你了一viewHolder而不是原来的convertView。
+         * @param holder
+         * @param position
+         */
         @Override
-        public void onBindViewHolder(MyViewHolder holder, int position)
-        {
+        public void onBindViewHolder(MyViewHolder holder, int position) {
             holder.tv.setText(mDatas.get(position));
         }
 
+        /**
+         * 这个方法就类似于BaseAdapter的getCount方法了，即总共有多少个条目。
+         * @return
+         */
         @Override
-        public int getItemCount()
-        {
+        public int getItemCount() {
             return mDatas.size();
         }
 
-        class MyViewHolder extends RecyclerView.ViewHolder
-        {
+        class MyViewHolder extends RecyclerView.ViewHolder {
 
             TextView tv;
 
-            public MyViewHolder(View view)
-            {
+            public MyViewHolder(View view) {
                 super(view);
                 tv = (TextView) view.findViewById(R.id.id_num);
             }
